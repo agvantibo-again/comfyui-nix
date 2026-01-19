eb9a7070742c697f23efcae84f718226c909a77a
Savchenko Dmitriy <apicalium@gmail.com>
2026-01-20 02:35:30 +0300

    Revert "Trim custom packages, remove cache"
    This reverts commit 4f6ca18d4c6543d4211a6c724da0f531667cb023.

--- flake.nix
+++ flake.nix
@@ -3,12 +3,12 @@
 
   nixConfig = {
     extra-substituters = [
-      # "https://comfyui.cachix.org"
+      "https://comfyui.cachix.org"
       "https://nix-community.cachix.org"
       "https://cuda-maintainers.cachix.org" # Legacy, still works
     ];
     extra-trusted-public-keys = [
-      # "comfyui.cachix.org-1:33mf9VzoIjzVbp0zwj+fT51HG0y31ZTK3nzYZAX0rec="
+      "comfyui.cachix.org-1:33mf9VzoIjzVbp0zwj+fT51HG0y31ZTK3nzYZAX0rec="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
